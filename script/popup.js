eb34f22c612f8e5fb3611f1102b2ef319303affc
AmorKor <mr.droix@gmail.com>
2020-09-22 21:29:16 +0300

    minor fixes

--- script/popup.js
+++ script/popup.js
@@ -4,13 +4,14 @@ const color = document.getElementById('currentColor')
 const colorText = color.textContent
 let position = 0
 let positionShift = popup.clientHeight
-console.log(colorText)
+// console.log(colorText)
 
 copyBtn.addEventListener('click', function() {
     var currentText = document.querySelector('#currentColor')
     
     if(currentText.textContent == colorText){
         popup.textContent = 'Generate color first'
+        // positionShift = popup.clientHeight
         
         animation()
     } else {
@@ -22,7 +23,7 @@ copyBtn.addEventListener('click', function() {
 })
 
 const animation = function() {
-    position += positionShift * 2
+    position += positionShift * 3
     popup.style.transform = `translateY(${position}px)`
 
     position = 0
