ea5cb9e1330f7120572c50a37f6e36b2ba720f1e
AmorKor <mr.droix@gmail.com>
2020-09-23 00:46:52 +0300

    minor fixes

--- script/copyBtn.js
+++ script/copyBtn.js
@@ -6,7 +6,6 @@ copyBtn.addEventListener('click', function() {
     var color = document.getElementById('currentColor').innerText
 
     textToClipboard(color)
-    activeScale()
 })
 
 //FUNCS
@@ -17,12 +16,4 @@ const textToClipboard = function(text) {
     dummy.select();
     document.execCommand("copy");
     document.body.removeChild(dummy);
-}
-
-const activeScale = function() {
-    copyBtn.style.transform = `scale(1.1)`
-
-    setTimeout(function() {
-        copyBtn.style.transform = `scale(1)`        
-    }, 300)
 }
--- script/scrollLock.js
+++
@@ -1,9 +0,0 @@
-const bodyScrollLock = require('body-scroll-lock');
-const disableBodyScroll = bodyScrollLock.disableBodyScroll;
-const enableBodyScroll = bodyScrollLock.enableBodyScroll;
-
-const body = document.querySelector('#body')
-
-disableBodyScroll(targetElement);
- 
-// enableBodyScroll(targetElement);
