3032a079a4810514748c336a31336630d41ea213
AmorKor <mr.droix@gmail.com>
2020-09-27 20:04:43 +0300

    fixed popup

--- script/popup.js
+++ script/popup.js
@@ -2,27 +2,25 @@
 const popup = document.getElementById('popup')
 const color = document.getElementById('currentColor')
 const colorText = color.textContent
-let position = 0
-let positionShift = popup.clientHeight
-// console.log(colorText)
 
 copyBtn.addEventListener('click', function() {
     var currentText = document.querySelector('#currentColor')
     
     if(currentText.textContent == colorText){
         popup.textContent = 'Generate color first'
-        // positionShift = popup.clientHeight
         
         animation()
     } else {
         popup.textContent = 'Copied to clipboard'
         
         animation()
-    }
-    
+    }    
 })
 
 const animation = function() {
+    let position = 0
+    let positionShift = popup.clientHeight
+
     position += positionShift * 3
     popup.style.transform = `translateY(${position}px)`
 
